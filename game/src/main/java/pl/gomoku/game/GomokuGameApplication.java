8f346b0a23163e3e84d6246976c527a8c6279755
Ksieciuniu17 <chrzanowski.krzysztof@interia.eu>
2025-05-29 15:04:02 +0200

    first commit with project files from spring.io project

---
+++ game/src/main/java/pl/gomoku/game/GomokuGameApplication.java
@@ -0,0 +1,13 @@
+package pl.gomoku.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomokuGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GomokuGameApplication.class, args);
+	}
+
+}
---
+++ game/src/test/java/pl/gomoku/game/GomokuGameApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gomoku.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomokuGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
